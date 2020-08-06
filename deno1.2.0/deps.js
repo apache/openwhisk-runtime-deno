305958fc2336e318197b5c9e4431313cddd6b3d8
Satya Rohith <me@satyarohith.com>
2020-08-06 17:35:22 +0530

    cache deps of compiler and launcher

---
+++ deno1.2.0/deps.js
@@ -0,0 +1,7 @@
+export {move, exists} from 'https://deno.land/std@0.63.0/fs/mod.ts';
+export {readLines} from 'https://deno.land/std@0.63.0/io/bufio.ts';
+export {
+  join,
+  fromFileUrl,
+  dirname
+} from 'https://deno.land/std@0.63.0/path/mod.ts';
