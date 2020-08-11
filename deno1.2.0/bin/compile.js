6ab4210889394f1d694a6d4598b9a27b6e84d161
Satya Rohith <me@satyarohith.com>
2020-08-11 14:02:50 +0530

    Do not compile bundles again

--- deno1.2.0/bin/compile.js
+++ deno1.2.0/bin/compile.js
@@ -1,36 +1,49 @@
 import {move, exists, join, fromFileUrl, dirname} from '../deps.js';
 
+async function isBundle(file) {
+  return (await Deno.readTextFile(file)).includes(
+    'export default __exp["default"];'
+  );
+}
+
 try {
   const srcDir = join(Deno.cwd(), '..', Deno.args[1]);
   const binDir = join(Deno.cwd(), '..', Deno.args[2]);
-
-  const sourceEntry = join(srcDir, 'main.ts');
   const execPath = join(srcDir, 'exec');
+  let sourceEntry = join(srcDir, 'main.ts');
+  let compiled = false;
 
   // Restructure files.
   if (await exists(execPath)) {
+    compiled = await isBundle(execPath);
+    if (compiled) {
+      sourceEntry = join(srcDir, 'ow_bundle.js');
+    }
+
     await move(execPath, sourceEntry, {
       overwrite: true
     });
   }
 
-  const process = Deno.run({
-    cmd: [
-      'deno',
-      'bundle',
-      '--unstable',
-      sourceEntry,
-      join(srcDir, 'ow_bundle.js')
-    ],
-    stderr: 'piped',
-    stdout: 'piped'
-  });
+  if (!compiled) {
+    const process = Deno.run({
+      cmd: [
+        'deno',
+        'bundle',
+        '--unstable',
+        sourceEntry,
+        join(srcDir, 'ow_bundle.js')
+      ],
+      stderr: 'piped',
+      stdout: 'piped'
+    });
 
-  const {code} = await process.status();
+    const {code} = await process.status();
 
-  if (code !== 0) {
-    const rawError = await process.stderrOutput();
-    console.log(new TextDecoder().decode(rawError));
+    if (code !== 0) {
+      const rawError = await process.stderrOutput();
+      console.log(new TextDecoder().decode(rawError));
+    }
   }
 
   // create /exec file
